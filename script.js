6602a987e084112f0c6a64f24cef0eb939bf89eb
naomichoe <114212195+naomichoe@users.noreply.github.com>
2022-12-15 01:55:58 -0800

    Added media queries for mobile and smaller desktop size

--- script.js
+++ script.js
@@ -14,20 +14,3 @@ for (i = 0; i < accordion.length; i++) {
 }
 
 
-// for (i = 0; i < accordion.length; i++) {
-//     accordion[i].addEventListener('click', function() {
-//         this.classList.toggle('active');
-//         let panel = this.nextElementSibling;
-//         if (panel.style.display === 'block') {
-//             panel.style.display = 'none';
-//         } else if (panel.style.display === 'none') {
-//             let active = document.querySelectorAll('.accordion.active');
-//             console.log(active);
-//             for (let j = 0; j < active.length; j++) {
-//                 active[j].classList.remove("active");
-//                 active[j].panel.style.display = 'none';
-//             }
-//             panel.style.display = 'block';
-//         }
-//     });
-// }
